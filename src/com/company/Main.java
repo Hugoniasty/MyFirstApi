6836d63cb16e658cd4ca157deb9bef28153baa6e
Hugoniasty <mwitkowski96@o2.pl>
2019-06-01 19:59:18 +0200

    Add checking test for data read from dtb

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,33 @@
 package com.company;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class Main {
     public static void main (String[] args) {
-        //write your code here
-        User user = new User ("9Hugo", "9Hugo@hugo.pl", "Hugo");
+        //new user to database
+/*      User user = new User ("9Hugo", "9Hugo@hugo.pl", "Hugo");
         UserDao userDao = new UserDao();
         userDao.create(user);
         System.out.println(user.getId());
+*/
+
+
+        //read user from database
+/*        UserDao userDao = new UserDao();
+        User user2 = userDao.read(12);
+        System.out.println(user2.getId());
+        System.out.println(user2.getUserName());
+        System.out.println(user2.getEmail());
+        System.out.println(user2.getPassword());
+
+        System.out.println("________");
+
+        if (userDao.read(12)==null) {
+            System.out.println("null");
+        } else if (userDao.read(12)!=null) {
+            System.out.println("not null");
+        }
+*/
     }
 }
